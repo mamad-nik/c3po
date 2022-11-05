7db054a7ea7eee90c5d93d036dbda283db864772
mamad-nik <khoshkhab11@gmail.com>
2022-11-06 00:19:49 +0330

    fixed bug

--- fileHandler/filereader.go
+++ fileHandler/filereader.go
@@ -18,12 +18,17 @@ func Read(fileName string) []string {
 	temp := ""
 	for i := 0; i < len(data)-1; i++ {
 		WS := string(data[i]) == " " || string(data[i]) == "\n" || string(data[i]) == "\t"
+		Para := string(data[i]) == "(" || string(data[i]) == ")" || string(data[i]) == "[" || string(data[i]) == "]" || string(data[i]) == "{" || string(data[i]) == "}"
 		if WS {
 			appender(&token, temp)
 			temp = ""
 		} else if string(data[i]) == ";" {
 			appender(&token, temp)
 			appender(&token, ";")
+			temp = ""
+		} else if Para {
+			appender(&token, temp)
+			appender(&token, string(data[i]))
 			temp = ""
 		} else {
 			temp = temp + string(data[i])
